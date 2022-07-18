19a4cb61e95a4df1c605f3bfa4ad7299e8fbd551
nirdesh <nirdeshk17@gmail.com>
2022-07-18 13:05:54 +0530

    edit_address

--- lib/screens/menu/address/edit_address.dart
+++ lib/screens/menu/address/edit_address.dart
@@ -78,10 +78,226 @@ class EditAddress extends ConsumerWidget {
                     height: 15,
                   ),
                   Padding(
-                    padding: EdgeInsets.only(left: 30),
+                    padding: EdgeInsets.only(left: 10),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        TextView("Full name(first name and last name)*",color: Palette.hintColor,),
+                        const PrimaryTextFormField(
+                          label: 'Full name (first name and last name)*',
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(10),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.zero,
+                            constraints:
+                            const BoxConstraints(maxHeight: 50, minWidth: 50),
+                            labelText: 'Phone number*',
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.only(left: 15, right: 5),
+                              child: CountryCodePicker(
+                                initialSelection: 'in',
+                                textStyle: TextStyle(
+                                  color: Palette.textColor,
+                                  fontFamily: GoogleFonts.lato().fontFamily,
+                                  fontSize: 16,
+                                  letterSpacing: 0,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                                flagWidth: 20,
+                                showDropDownButton: true,
+                                flagDecoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(4)),
+                              ),
+                            ),
+                            prefixIconConstraints: const BoxConstraints(),
+                            labelStyle: TextStyle(
+                              color: Palette.hintColor,
+                              fontFamily: GoogleFonts.lato().fontFamily,
+                              fontSize: 16,
+                              letterSpacing: 0,
+                              fontWeight: FontWeight.normal,
+                              height: 1.5,
+                            ),
+                            floatingLabelStyle: TextStyle(
+                              color: Palette.hintColor,
+                              fontFamily: GoogleFonts.lato().fontFamily,
+                              fontSize: 18,
+                              letterSpacing: 0,
+                              fontWeight: FontWeight.normal,
+                              height: 1.5,
+                            ),
+                            border: WidgetStyle.inputBorder,
+                            enabledBorder: WidgetStyle.inputBorder,
+                            focusedBorder: WidgetStyle.activeInputBorder,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(10),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.zero,
+                            constraints:
+                            const BoxConstraints(maxHeight: 50, minWidth: 50),
+                            labelText: 'Alternate phone number',
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.only(left: 15, right: 5),
+                              child: CountryCodePicker(
+                                initialSelection: 'in',
+                                textStyle: TextStyle(
+                                  color: Palette.textColor,
+                                  fontFamily: GoogleFonts.lato().fontFamily,
+                                  fontSize: 16,
+                                  letterSpacing: 0,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                                flagWidth: 20,
+                                showDropDownButton: true,
+                                flagDecoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(4)),
+                              ),
+                            ),
+                            prefixIconConstraints: const BoxConstraints(),
+                            labelStyle: TextStyle(
+                              color: Palette.hintColor,
+                              fontFamily: GoogleFonts.lato().fontFamily,
+                              fontSize: 16,
+                              letterSpacing: 0,
+                              fontWeight: FontWeight.normal,
+                              height: 1.5,
+                            ),
+                            floatingLabelStyle: TextStyle(
+                              color: Palette.hintColor,
+                              fontFamily: GoogleFonts.lato().fontFamily,
+                              fontSize: 18,
+                              letterSpacing: 0,
+                              fontWeight: FontWeight.normal,
+                              height: 1.5,
+                            ),
+                            border: WidgetStyle.inputBorder,
+                            enabledBorder: WidgetStyle.inputBorder,
+                            focusedBorder: WidgetStyle.activeInputBorder,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        const PrimaryTextFormField(
+                          label: 'Pin Code',
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        const PrimaryTextFormField(
+                          label: 'Address*',
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        const PrimaryTextFormField(
+                          label: 'Landmark',
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        const PrimaryTextFormField(
+                          label: 'City',
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        const PrimaryTextFormField(
+                          label: 'State',
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextView("Save Address as",color: Palette.hintColor,),
+                        Row(
+                          children: [
+                            Container(
+                              child: Row(
+                                children: [
+                                  Radio(
+                                      value: false, groupValue: true, onChanged: (value) {}),
+                                  Text("Home"),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                              child: Row(
+                                children: [
+                                  Radio(
+                                      value: false, groupValue: true, onChanged: (value) {}),
+                                  Text("Work"),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                              child: Row(
+                                children: [
+                                  Radio(
+                                      value: true, groupValue: true, onChanged: (value) {}),
+                                  Text("Other"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "Address"
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(value: true, onChanged:(value){},activeColor: Colors.white,checkColor: Colors.black,),
+                            TextView("Use as my default",color: Palette.hintColor,),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            PrimaryButton(title: "SAVE & REQUEST CHANGE",onPressed: (){},),
+                          ],
+                        ),
+SizedBox(
+  height: 10,
+),
+                        Text("Pride of Cows team will review your request to change this address and implement it at the earliest.",style: TextStyle(fontSize: 17),),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            PrimaryButton(title: "DISCARD CHANGES",onPressed: (){},colorFill: false,),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
                       ],
                     ),
                   ),
